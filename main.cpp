fdf664b43e729799523a4ea7649f5233024588ab
RagingDepression <raijon21@gmail.com>
2025-09-25 19:47:10 -0400

    must finish

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // library to get inputs and outputs
+using namespace std;
+int main() 
+{
+     string name;
+    cout << "Enter Your Name:";
+    cin >>
+    // std::getline (std::cin, name) ;
+    // std:: string favorite;
+    // std:: cout <<"Whats your favorite ice cream?";
+    // std::getline (std::cin, favorite);
+    // std:: string hours;
+    // std:: cout << "How many. hours do you wanna stay asleep?";
+    // std::getline(std::cin, hours);
+
+
+
+    return 0;
+}
